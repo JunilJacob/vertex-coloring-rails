9e3a28625c2d54b2cd911e674692237b43a5f498
JunilJacob <juniljacob124@gmail.com>
2013-02-23 23:32:54 +0530

    complete

---
+++ vertex coloring in rails/app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+  def post
+	require 'json'
+	require 'set'
+	data=params["data"]
+	adj_list=JSON.load(data)
+	colors=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+	color_list=[-1]*adj_list.length
+	0.upto adj_list.length-1 do |vertex|
+		adj_vertices=adj_list[vertex]
+		adj_colors=[]
+		adj_vertices.each do |adj|
+			adj_colors.push(color_list[adj])
+		end	
+		color_list[vertex]=(colors.to_set-adj_colors.to_set).to_a[0]
+	end
+	color_string = JSON.dump(color_list)
+	 render :json => color_string
+  end
+end
---
+++ vertex coloring in rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaintApp::Application.config.secret_token = '5d04b5425de4c6916b41db8549143cd3120d7a06b54fc7e18283445f9bcb04e496f234c6de02b29a13e6b3c871b92ea110987fb6e76542a73983bb77885ded21'
